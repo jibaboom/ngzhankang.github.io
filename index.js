b95568f06eaebefaece37576f885e2325cffaa01
ngzhankang <ngzhankang@outlook.com>
2022-08-10 12:21:42 +0000

    Revamped Front Page and Boiler Template.

---
+++ index.js
@@ -0,0 +1,22 @@
+// function dropdown () {
+//     const navs = document.querySelectorAll('.nav-group')
+
+//     navs.forEach(nav => {
+//         if (nav.classList.contains('nav-expanded')) {
+//             nav.classList.remove('nav-expanded')
+//             nav.classList.add('nav-collapsed')
+//         }
+//         else {
+//             if (nav.classList.contains('nav-collapsed')) {
+//                 nav.classList.remove('nav-collapsed');
+//             }
+//             else {
+//                 nav.classList.add('nav-expanded');
+//             }
+//         }
+//     });
+// }
+
+// document
+// .querySelector('.nav-toggle')
+// .addEventListener('click', dropdown)
--- site.hs
+++ site.hs
@@ -1,25 +1,35 @@
 --------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
+import Control.Monad (liftM)
+import qualified Data.ByteString.Lazy.Char8 as C
 import           Data.Monoid (mappend)
 import           Hakyll
+import           Text.Jasmine
 
 
 --------------------------------------------------------------------------------
 config :: Configuration
-config = defaultConfiguration
-  { 
-    destinationDirectory = "docs"
-  }
-  
+config = defaultConfiguration { 
+    destinationDirectory = "_site"
+}
+
+
+compressJsCompiler :: Compiler (Item String)
+compressJsCompiler = do
+    let minifyJS = C.unpack . minify . C.pack . itemBody
+    s <- getResourceString
+    return $ itemSetBody (minifyJS s) s
+
+
 main :: IO ()
 main = hakyllWith config $ do
-    match "images/*" $ do
+    match "images/favicon/*" $ do
         route   idRoute
         compile copyFileCompiler
 
-    match "css/*" $ do
+    match "images/others/*" $ do
         route   idRoute
-        compile compressCssCompiler
+        compile copyFileCompiler
 
     match (fromList ["about.rst", "contact.markdown"]) $ do
         route   $ setExtension "html"
@@ -34,8 +44,24 @@ main = hakyllWith config $ do
             >>= loadAndApplyTemplate "templates/default.html" postCtx
             >>= relativizeUrls
 
-    create ["archive.html"] $ do
+    match "css/default.scss" $
+        compile $ liftM (fmap compressCss) $
+            getResourceFilePath
+            >>= \fp -> unixFilter "sass" ["--scss", fp] ""
+            >>= makeItem
+
+    create ["default.css"] $ do
         route idRoute
+        compile $ do
+            items <- loadAll "css/*"
+            makeItem $ concatMap itemBody (items :: [Item String])
+
+    match "index.js" $ do
+        route idRoute
+        compile compressJsCompiler
+
+    create ["archive.html"] $ do
+        route   idRoute
         compile $ do
             posts <- recentFirst =<< loadAll "posts/*"
             let archiveCtx =
@@ -48,14 +74,13 @@ main = hakyllWith config $ do
                 >>= loadAndApplyTemplate "templates/default.html" archiveCtx
                 >>= relativizeUrls
 
-
     match "index.html" $ do
-        route idRoute
+        route   idRoute
         compile $ do
             posts <- recentFirst =<< loadAll "posts/*"
             let indexCtx =
                     listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Home"                `mappend`
+                    constField "title" "Welcome to my personal space!"           `mappend`
                     defaultContext
 
             getResourceBody
